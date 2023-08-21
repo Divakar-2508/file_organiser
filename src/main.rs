b31bd6a54fc56ac2821ed3b44fa1f0aa398ea1ae
Divakar-2508 <divakaraugust@gmail.com>
2023-08-21 15:47:22 +0530

    Filtered files from the directory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 #![allow(non_snake_case)]
+use std::{env, path::Path};
 
 fn main() {
+    let curr_dirr = env::current_dir().expect("Can't read directory");
     
+    let files = curr_dirr.read_dir()
+        .expect("Cant' take files")
+        .filter_map(|entry|{
+            let path = entry.unwrap().path();
+            if path.is_file(){
+                Some(path)
+            } else {
+                None
+            }
+        });
+    
+    for file in files{
+        println!("{:?}", file);
+    }
 }
