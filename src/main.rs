e47eaad64ae083d07a0782c3e344a731b130a921
Divakar-2508 <divakaraugust@gmail.com>
2023-08-22 18:41:06 +0530

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
-use std::env;
-use std::fs;
+use std::{env,fs};
+use std::io::Write;
 
 fn main() {
     let curr_dirr = env::current_dir().expect("Can't read directory");
@@ -26,5 +26,7 @@ fn main() {
 
         let new_path = folder_name.join(file.file_name().unwrap());
         fs::rename(&file, &new_path).expect("Can't rename file");
+        println!("Files Organized!!");
+        std::io::stdout().flush().unwrap();
     }
 }
