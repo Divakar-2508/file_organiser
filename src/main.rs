09b1bb3c8dc3837133397c307d39f84d14a2a8d5
Divakar-2508 <divakaraugust@gmail.com>
2023-08-31 21:48:09 +0530

    Added Some comments

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ use std::io::Write;
 //Example: file_organizer /home/user/Downloads
 fn main() {
 
-
+    //Getting the arguments from the command line.
     let enb = env::args().collect::<Vec<String>>();
     let curr_dirr = env::current_dir().expect("Can't read directory");
 
     let dir;
 
+    //Checking if the user has provided a path or not.
     if enb.len() > 1 {
         if Path::new(&enb[1]).is_dir() {
             dir = Path::new(&enb[1]);
@@ -26,7 +27,6 @@ fn main() {
             return;
         }
     } else {
-
         print!("Do you want to Organise the current Directory? (y/n): ");
         std::io::stdout().flush().unwrap();
 
@@ -42,10 +42,12 @@ fn main() {
         }
     }
 
+    //Filter files from the directory.
     let files = dir
         .read_dir()
         .expect("Cant' take files")
         .filter_map(|entry| {
+            //Filtering out the files using is_file() method.
             let path = entry.unwrap().path();
             if path.is_file() {
                 Some(path)
@@ -54,15 +56,20 @@ fn main() {
             }
         });
 
+    //Looping thru the files and moving them to their respective folders.
     for file in files {
         if let Some(extension) = file.extension() {
+
+            //Getting the extension and creating the folder name.
             let file_extension = extension.to_str().unwrap();
             let folder_name = dir.join(file_extension);
 
+            //Creating the folder if it doesn't exist.
             if !folder_name.exists() {
                 std::fs::create_dir(&folder_name).expect("Can't create folder");
             }
 
+            //Renaming the file to move it to the folder.
             let new_path = folder_name.join(file.file_name().unwrap());
             fs::rename(&file, &new_path).expect("Can't rename file");
         }
